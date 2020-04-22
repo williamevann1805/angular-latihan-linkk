c2b249dc5ba24fce5b973571ef9032f2368d8297
williamevann1805 <c14180222@john.petra.ac.id>
2020-04-22 10:12:21 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { InputComponent } from './input/input.component';
 import { ListComponent } from './list/list.component';
 import { EditComponent } from './edit/edit.component';
 import{Routes, RouterModule } from '@angular/router';
+import { LinkComponent } from './link/link.component';
 
 const ROUTES:Routes = [
   { path:'input', component : InputComponent },
@@ -21,7 +22,7 @@ const ROUTES:Routes = [
   FormsModule,
   RouterModule.forRoot(ROUTES)
   ],
-  declarations: [ AppComponent, HelloComponent, InputComponent, ListComponent, EditComponent ],
+  declarations: [ AppComponent, HelloComponent, InputComponent, ListComponent, EditComponent, LinkComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
